4ce7182b7f843057df2bc391be588e3f662f37f5
Nosakeys <silaskeys95@gmail.com>
2023-04-04 10:30:07 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,33 @@
+// Mr John
+
+
+// let car = {
+//     no_wheel: 4,
+//     colour: "red",
+//     speed: 45,
+//     manufacturers : [
+//         "Ford",
+//         "Toyota",
+//         "Hyundi",
+//         "Lexus",
+//     ],
+//     info : function(){
+//         console.log( `My car is ${this.colour} and it has the speed of ${this.speed}`)
+//     }
+    
+// }
+// console.log(car.no_wheel);
+// console.log(car.colour);
+// console.log(car.speed);
+// console.log(car.manufacturers);
+// console.log(car.info());
+
+
+// let board ={};
+
+// board.colour = "pink",
+// board.height = 35,
+// board.shape = "rectangle",
+// board.width ="30"
+
+// console.log (board)
